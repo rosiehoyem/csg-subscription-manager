f206598315dc6535d2c64b88dd83ce3ec54c8fe8
Rosie Hoyem <rosiehoyem@gmail.com>
2014-06-22 16:24:07 -0500

    added bootstrap and tweaked views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'solar_gardens#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   resources :subscribers
 
   resources :solar_gardens
 
-  root 'solar_gardens#index'
+ 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
